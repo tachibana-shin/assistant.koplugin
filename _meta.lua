bbb0e7189e6eaaa0626eb939f348164794653ee4
omer-faruq <85738646+omer-faruq@users.noreply.github.com>
2025-06-10 12:19:07 +0300

    v 1.05

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "assistant",
     fullname = "Assistant",
     description = _([[Allows the user to query an AI API for answers to questions about highlighted text.]]),
-    version = 1.04,
+    version = 1.05,
 }
